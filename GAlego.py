f5f44c4f528173e624042915af2f1194061284e5
Elia <elia7amar@gmail.com>
2019-02-19 21:36:01 +0200

    Combined file - GAlego, and parameters changed

--- GAlego.py
+++ GAlego.py
@@ -1,8 +1,8 @@
 import random
 import sys
 import copy
-import gui
 import threading
+import pygame
 
 # Define the size of the boards
 N = 10
@@ -20,7 +20,7 @@ def main():
     population = Population()
 
     # Show the result board in another thread (interferes with the printing)
-    gui_thread = threading.Thread(target=gui.gui(population.resultBoard))
+    gui_thread = threading.Thread(target=gui(population.resultBoard))
     gui_thread.start()
 
     # Restore standard output to screen
@@ -395,6 +395,96 @@ class Population:
                 nextGeneration.append(father)
 
         return nextGeneration
+
+def gui(board):
+    # Define some colors
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GREEN = (0, 255, 0)
+    RED = (255, 0, 0)
+
+    # This sets the WIDTH and HEIGHT of each grid location
+    WIDTH = 30
+    HEIGHT = 30
+
+    # This sets the margin between each cell
+    MARGIN = 1
+
+    # Set row 1, cell 5 to one.
+    # (Remember rows and column numbers start at zero.)
+    #grid[1][5] = 1
+
+    # Initialize pygame
+    pygame.init()
+
+    # Set the HEIGHT and WIDTH of the screen
+    WINDOW_SIZE = [311, 311]
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+
+    # Set title of screen
+    pygame.display.set_caption("Final Board : " + str(board.capacity) + "% full")
+
+    # Loop until the user clicks the close button.
+    done = False
+
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+
+    # -------- Main Program Loop -----------
+    while not done:
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True  # Flag that we are done so we exit this loop
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+                # User clicks the mouse. Get the position
+    #            pos = pygame.mouse.get_pos()
+                # Change the x/y screen coordinates to grid coordinates
+    #            column = pos[0] // (WIDTH + MARGIN)
+    #            row = pos[1] // (HEIGHT + MARGIN)
+                # Set that location to one
+    #            grid[row][column] = 1
+    #            print("Click ", pos, "Grid coordinates: ", row, column)
+
+
+        # Set the screen background
+        screen.fill(BLACK)
+
+        # Draw the grid
+        for row in range(10):
+            for column in range(10):
+                color = WHITE
+                if board.board[row][column] != "":
+                    brickType = ord(board.board[row][column][:-1]) - ord('a')
+                    if brickType == 0:      # a
+                        color = (255, 0, 0)
+                    elif brickType == 1:    # b
+                        color = (0, 255, 0)
+                    elif brickType == 2:    # c
+                        color = (0, 0, 255)
+                    elif brickType == 3:    # d
+                        color = (255, 255, 0)
+                    elif brickType == 4:    # e
+                        color = (0, 255, 255)
+                    elif brickType == 5:    # f
+                        color = (255, 0, 255)
+
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN,
+                                  (MARGIN + HEIGHT) * row + MARGIN,
+                                  WIDTH,
+                                  HEIGHT])
+
+        # Limit to 60 frames per second
+        clock.tick(60)
+
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+
+    # Be IDLE friendly. If you forget this line, the program will 'hang'
+    # on exit.
+    pygame.quit()
 
 
 # Upon run call 'main'
